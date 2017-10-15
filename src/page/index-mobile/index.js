c8ac9e139926580626bd6beef84284e7722753f9
ssunj <1651271841@qq.com>
2017-10-15 18:50:02 +0800

    There are two different interfaces to split the phone and the computer

---
+++ src/page/index-mobile/index.js
@@ -0,0 +1,17 @@
+function IndexController() {
+	var self = this;
+
+	self._init = function(canvas) {
+		
+
+	};
+
+	self._canvasSquare = function() {
+
+
+
+	};
+
+}
+if (typeof(indexController) == "undefined") {}
+indexController = new IndexController();
